8d29f8a8b68f35de50e82ada6809847f0ec27a48
huanghongru <onedesire@sjtu.edu.cn>
2019-10-14 13:08:41 +0800

    add label

--- scripts/process_snli.py
+++ scripts/process_snli.py
@@ -5,18 +5,19 @@ train = '/home/hongru/fairseq/snli/snli_1.0_train.jsonl'
 valid = '/home/hongru/fairseq/snli/snli_1.0_dev.jsonl'
 test = '/home/hongru/fairseq/snli/snli_1.0_test.jsonl'
 
-def preprocess(in_file, pre_file, hyp_file):
-	pre = open(pre_file, 'w')
-	pre.close()
-	hyp = open(hyp_file, 'w')
-	hyp.close()
+def preprocess(in_file, pre_file, hyp_file, label):
+    pre = open(pre_file, 'w')
+    pre.close()
+    hyp = open(hyp_file, 'w')
+    hyp.close()
 
-	pre = open(pre_file, 'a')
-	hyp = open(hyp_file, 'a')
-	with jsonlines.open(in_file) as reader:
-		for datum in reader:
-			pre.write(' '.join(datum['sentence1'].lower()[:-1].split() + ['.'])+'\n')
-			hyp.write(' '.join(datum['sentence2'].lower()[:-1].split() + ['.'])+'\n')
+    pre = open(pre_file, 'a')
+    hyp = open(hyp_file, 'a')
+    with jsonlines.open(in_file) as reader:
+        for datum in reader:
+            if datum['label'] == label:
+                pre.write(' '.join(datum['sentence1'].lower()[:-1].split() + ['.'])+'\n')
+                hyp.write(' '.join(datum['sentence2'].lower()[:-1].split() + ['.'])+'\n')
 
 
 dir_ = '/home/hongru/fairseq/snli-tokenized-en-de'
@@ -28,6 +29,6 @@ test_pre = 'test.pre'
 test_hyp = 'test.hyp'
 
 if not os.path.exists(dir_):
-	os.mkdir(dir_)
+    os.mkdir(dir_)
 
-preprocess(test, os.path.join(dir_, test_pre), os.path.join(dir_, test_hyp))
+preprocess(test, os.path.join(dir_, test_pre), os.path.join(dir_, test_hyp), 'entailment')
